33ee3159fbe5e5852f5afb9aeea76c6eec94ba4d
Evgenii Pavlov <mossad@avride.ai>
2024-03-03 18:45:12 +0200

    refactor

--- check_pid_json_file.py
+++ check_pid_json_file.py
@@ -1,38 +1,45 @@
 import json
-from check_prime import check_prime
-
-math_eng_ru_translation = {
-    False: "Cоставное",
-    True: "Простое",
-    "1": "ни простое ни составное"
-}
-with open("process_output.json", "r+") as file:
-
-    file_check = json.load(file)  # <class 'list'>
+from modules.is_num_prime import is_num_prime
 
 
-for key in file_check:
-
-    process_number = key["Process Number"]
-    process_name = key["Process Name"]
-
-    result_before_check = key["Is Prime"]
-    
-     
-    
-
-    result_after_check = check_prime(process_number)
+def prime_sign_to_ru_output(is_prime, number_to_check):
+    if is_prime:
+        return "простое"
+    else:
+        if number_to_check == 0 or number_to_check == 1:
+            return "ни простое, ни составное"
+        return "составное"
 
 
+if __name__ == '__main__':
+    with open("process_output.json", "r") as file_with_processes_data:
+        processes_data = json.load(file_with_processes_data)
 
-    if result_before_check != result_after_check: # True неравно False "
-        if process_number == 1:
-            result_after_check = "1"
-        print(
-            "Pid процесс с именем {0} проверен. Найдена ошибка. Число {1} на самом деле {2}, а в файле написано {3}".format(
-                process_name, # systemd
-                process_number, # 1
-                math_eng_ru_translation[result_after_check].lower(), # 1
-                math_eng_ru_translation[result_before_check].lower(),  # True в простое
+    errors_found = 0
+    for process_data in processes_data:
+        process_id = process_data["Process ID"]
+        process_name = process_data["Process Name"]
+
+        ru_prime_sign_data_in_file = prime_sign_to_ru_output(process_data["Is Prime"], process_id)
+        ru_prime_sign_data_actual = prime_sign_to_ru_output(is_num_prime(process_id), process_id)
+
+        if ru_prime_sign_data_in_file != ru_prime_sign_data_actual:
+            errors_found += 1
+
+            print(
+                "Для {0} процесса найдена ошибка в файла. "
+                "Число(PID) {1} на самом деле {2}, "
+                "а в файле написано {3}.\n"
+                .format(
+                    process_name,
+                    process_id,
+                    ru_prime_sign_data_actual,
+                    ru_prime_sign_data_in_file,
+                    errors_found
+                )
             )
-        )
+
+    if errors_found == 0:
+        print("Файл проверен. Ошибок не найдено")
+    else:
+        print("В файле найдены ошибки: {0} штуки".format(errors_found))
--- psutil_pid_create_json.py
+++ psutil_pid_create_json.py
@@ -1,47 +1,24 @@
 import psutil
 import json
-import math
-
-
-def check_prime(number_to_check):
-
-    initial_devider = 2
-    final_devider = math.isqrt(number_to_check)  # final_devider = 1
-    deviders = range(initial_devider, final_devider + 1)  # deviders = [2]
-    check_result = True
-    for current_devider in deviders:
-        if number_to_check % current_devider == 0:
-            check_result = False
-            break
-        else:
-            check_result = True
-
-    return check_result
-
+from modules.is_num_prime import is_num_prime
 
 process_list = []
 
 for process in psutil.process_iter(["pid", "name"]):
-
     process_name = process.info["name"]  # получаем столбец с именами процессов
     process_number = process.info["pid"]  # получаем столбец с номерами процессов
 
-    dict_in_file = {}
+    dict_in_file = {"Process Name": process_name, "Process ID": process_number}
 
-    dict_in_file["Process Name"] = process_name
-    dict_in_file["Process Number"] = process_number
     if process_number == 1:
         dict_in_file["Is Prime"] = False
-
     elif process_number == 2 or process_number == 3:
         dict_in_file["Is Prime"] = True
-
     else:
-        process_after_check_prime = check_prime(process_number)
+        process_after_check_prime = is_num_prime(process_number)
         dict_in_file["Is Prime"] = process_after_check_prime
 
     process_list.append(dict_in_file)
-
 
 with open("process_output.json", "w") as file:
     json.dump(process_list, file)
